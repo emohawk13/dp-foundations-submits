877443f8cac960ffd4d6ea19c47b7f55888d2498
dauhson <dauhson13@gmail.com>
2024-02-13 11:31:11 -0700

    inital commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# String Interpolation Assignment
+
+price1 = 125.5
+price2 = 75
+price3 = 3540.99
+
+# Program 1
+print("Start of program 1")
+print(f'Price1 is $ {price1:.2f}')
+print(f'Price2 is $ {price2:.2f}')
+print(f'Price3 is $ {price3:.2f}')
+print("end of program 1")
+print()
+# Program 2
+print("Start of program 2")
+print(f'Price1 is $ {price1:>7.2f}')
+print(f'Price2 is $ {price2:>7.2f}')
+print(f'Price3 is $ {price3:>7.2f}')
+print("end of program 2")
+print()
+# Program 3
+print("Start of program 3")
+print(f'Price1 is $ {price1:>8.2f}')
+print(f'Price2 is $ {price2:>8.2f}')
+print(f'Price3 is $ {price3:,.2f}')
+print("end of program 3")

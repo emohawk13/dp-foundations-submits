86f03da18bf4a87f7490a0d0b9252acd519e56b9
dauhson <dauhson13@gmail.com>
2024-02-13 11:38:26 -0700

    inital commit

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
-# Number guessing game
-import random
-
-def numbergen():
-    return random.randint(1, 100)
+# Leap Year Finder
 
 def player_input():
     while True:
         try:
-            user_input = int(input("Please choose a number from 1 to 100: "))
-            return user_input
-        except ValueError: 
-            print("Please use a number.")
+            user_input = int(input("Enter a year: "))
+            if user_input >= 1752:
+                return user_input
+            else:
+                print("Please use a year after 1752 AD.")
+        except ValueError:
+            print("Invalid input. Please enter a valid year.")
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
 def game_start():
-    target_number = numbergen()
-    print("Welcome to the number guessing game!")
-    guess_count = 0
-    
+    print("Welcome to Leap Year Finder")
     while True:
-        guess = player_input()
-        guess_count += 1
+        year = player_input()
+        result = is_leap_year(year)
+        print(result)
         
-        if guess < target_number:
-            print("Too low! Try again.")
-        elif guess > target_number:
-            print("Too high! Try again.")
-        else:
-            print(f"Congratulations! You guessed the correct number in {guess_count} guesses.")
+        continue_input = input("Would you like to continue? (yes/no): ").strip().lower()
+        if continue_input not in ["yes", "y", ""]:
             break
-        
-        if guess_count % 2 == 0 and guess_count != 2:
-            print(f"You've made {guess_count} guesses. Keep going!")
-        elif guess_count % 3 == 0 and guess_count != 2:
-            print(f"You've made {guess_count} guesses. Can you guess it?")
-        elif guess_count % 4 == 0 and guess_count != 2:
-            print(f"You've made {guess_count} guesses. You're getting closer!")
-        if guess_count % 5 == 0:
-            reveal_option = input(f"You've made {guess_count} attempts. Do you want to reveal the target number? (Type '0' to reveal and quit, or press Enter to continue): ")
-            if reveal_option == '0':
-                print(f"The target number was {target_number}. Thanks for playing!")
-                break
 
 game_start()
-
-

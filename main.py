e01e8a95529b6c7a31e2b45cfab8748f65c6cfb3
dauhson <dauhson13@gmail.com>
2024-02-13 11:37:24 -0700

    inital commit

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-# Christmas Tree Program
+# Number guessing game
+import random
 
-height = int(input("Enter the height of the tree: "))
-print("Here is your tree:")
+def numbergen():
+    return random.randint(1, 100)
 
-# For loop that takes the user input and builds the star pattern
-for i in range(1, height + 1):
-    print((" " * (height - i)) + ("* " * i))
-top_star_width = 2 * height - 1
+def player_input():
+    while True:
+        try:
+            user_input = int(input("Please choose a number from 1 to 100: "))
+            return user_input
+        except ValueError: 
+            print("Please use a number.")
 
-trunk_height = int(height / 2)
-trunk_width = int(height / 2 + 1)
-spaces_before_trunk = int((top_star_width - trunk_width) / 2)
+def game_start():
+    target_number = numbergen()
+    print("Welcome to the number guessing game!")
+    guess_count = 0
+    
+    while True:
+        guess = player_input()
+        guess_count += 1
+        
+        if guess < target_number:
+            print("Too low! Try again.")
+        elif guess > target_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the correct number in {guess_count} guesses.")
+            break
+        
+        if guess_count % 2 == 0 and guess_count != 2:
+            print(f"You've made {guess_count} guesses. Keep going!")
+        elif guess_count % 3 == 0 and guess_count != 2:
+            print(f"You've made {guess_count} guesses. Can you guess it?")
+        elif guess_count % 4 == 0 and guess_count != 2:
+            print(f"You've made {guess_count} guesses. You're getting closer!")
+        if guess_count % 5 == 0:
+            reveal_option = input(f"You've made {guess_count} attempts. Do you want to reveal the target number? (Type '0' to reveal and quit, or press Enter to continue): ")
+            if reveal_option == '0':
+                print(f"The target number was {target_number}. Thanks for playing!")
+                break
 
-for _ in range(trunk_height):
-    print((" " * spaces_before_trunk) + ("*" * trunk_width))
+game_start()
+
 
